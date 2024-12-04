b3e934c81792af62c1c3e1ac064982f9344a03ec
linzhe141 <1572213544@qq.com>
2024-12-04 10:27:42 +0800

    feat: add example github-pages

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -3,9 +3,21 @@ import vue from '@vitejs/plugin-vue'
 import { join } from 'path'
 
 export default defineConfig({
+  base: './',
+  build: {
+    minify: false,
+    rollupOptions: {},
+  },
   resolve: {
     // 直接使用源码
     alias: [
+      {
+        find: 'vue',
+        replacement: join(
+          __dirname,
+          '../node_modules/vue/dist/vue.esm-browser.js'
+        ),
+      },
       {
         find: 'vue-change-marker',
         replacement: join(__dirname, '../packages/core/index.ts'),
